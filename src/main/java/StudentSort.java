8297bbceefdd4e56ef784995f917869399ce9038
gaudvile <gaudvile@hotmail.com>
2022-02-09 16:05:43 +0200

    initial commit

--- src/main/java/StudentSort.java
+++ src/main/java/StudentSort.java
@@ -9,9 +9,9 @@ public class StudentSort {
     public static void main(final String[] args) {
 
         FileReader reader = new FileReader();
-        if (isValidAlgorithm("BUBBLE")) {
+        if (isValidAlgorithm(args[1])) {
             try {
-                sort(reader.readFile("input.txt"), "BUBBLE");
+                sort(reader.readFile(args[0]), args[1]);
             } catch (IOException e) {
                 e.printStackTrace();
             }
